4963adbb36466629a29e3a1614fd5ebd5e86bedf
TheOGZer0 <maartenjeu@gmail.com>
2021-06-02 20:02:54 +0200

    Add vlucht

---
+++ src/Vlucht.java
@@ -0,0 +1,26 @@
+public class Vlucht extends Stap{
+    private int price;
+    private double baggageLossChance;
+    private int baggagePrice;
+
+    public Vlucht(Plek a, Plek b, int price, int baggagePrice, double baggageLossChance){
+        super(a, b);
+        this.price = price;
+        this.baggagePrice = baggagePrice;
+        this.baggageLossChance = baggageLossChance;
+    }
+
+    public Vlucht(Plek a, Plek b, int price){
+        this(a, b, price, 0, 0.0);
+    }
+
+    @Override
+    public String toString() {
+        return a.toString() + " <--flight--> " + b.toString();
+    }
+
+    @Override
+    public double getWeight() {
+        return this.price + (this.baggagePrice * (this.baggageLossChance / 100));
+    }
+}
