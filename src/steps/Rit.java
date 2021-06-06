4d4438337cab8832013b53a9aeed51f6f685fbd8
TheOGZer0 <maartenjeu@gmail.com>
2021-06-06 13:50:58 +0200

    Add JavaDoc to Rit class, constructor, getWeight()

--- src/steps/Rit.java
+++ src/steps/Rit.java
@@ -3,9 +3,20 @@ package steps;
 import misc.DijkstraError;
 import places.Plek;
 
+/**
+ * Extension of {@link Stap}, that represent a car ride in kilometers.
+ */
 public class Rit extends Stap {
     private double distance; //In KiloMeter
 
+    /**
+     * Rit constructor.
+     *
+     * @param a used in {@link Stap#Stap(Plek, Plek)} call.
+     * @param b used in {@link Stap#Stap(Plek, Plek)} call.
+     * @param distance car ride distance in KiloMeter.
+     * @throws DijkstraError thrown by {@link Stap#verifyWeight()}.
+     */
     public Rit(Plek a, Plek b, double distance) throws DijkstraError{
         super(a, b);
         this.distance = distance;
@@ -17,6 +28,11 @@ public class Rit extends Stap {
         return a.toString() + " <--car ride--> " + b.toString();
     }
 
+    /**
+     * Get weight for use in Dijkstra's algorithm.
+     *
+     * @return distance in Km.
+     */
     @Override
     public double getWeight() {
         return this.distance;
