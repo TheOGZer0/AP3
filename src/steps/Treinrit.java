9c04fc3d5d24017a3e8194567485bffc9062fa40
TheOGZer0 <maartenjeu@gmail.com>
2021-06-06 13:46:44 +0200

    Add JavaDoc to Treinrit class, constructors, getWeight()

--- src/steps/Treinrit.java
+++ src/steps/Treinrit.java
@@ -3,10 +3,25 @@ package steps;
 import misc.DijkstraError;
 import places.Plek;
 
+
+/**
+ * Extension of {@link Stap}, that represent a train journey in minutes.
+ * Train journey can also have a current delay, which will be added to the weight
+ * returned by getWeight().
+ */
 public class Treinrit extends Stap {
     private final double journeyTime; //In minutes
     private double currentDelay; //In minutes
 
+    /**
+     * Treinrit constructor.
+     * 
+     * @param a used in {@link Stap#Stap(Plek, Plek)} call.
+     * @param b used in {@link Stap#Stap(Plek, Plek)} call.
+     * @param journeyTime amount of minutes this train journey takes.
+     * @param currentDelay amount of minutes of delay currently on this train journey.
+     * @throws DijkstraError thrown by {@link Stap#verifyWeight()}.
+     */
     public Treinrit(Plek a, Plek b, double journeyTime, double currentDelay) throws DijkstraError {
         super(a, b);
         this.journeyTime = journeyTime;
@@ -14,6 +29,11 @@ public class Treinrit extends Stap {
         this.verifyWeight();
     }
 
+    /**
+     * Treinrit constructor.
+     *
+     * Calls {@link Treinrit#Treinrit(Plek, Plek, double, double)} with currentDelay = 0.0
+     */
     public Treinrit(Plek a, Plek b, double journeyTime) throws DijkstraError{
         this(a, b, journeyTime, 0.0);
     }
@@ -25,6 +45,11 @@ public class Treinrit extends Stap {
         return a.toString() + " <--train Journey--> " + b.toString();
     }
 
+    /**
+     * Get weight for use in Dijkstra's algorithm.
+     *
+     * @return journeyTime + currentDelay.
+     */
     @Override
     public double getWeight() {
         return this.journeyTime + this.currentDelay;
