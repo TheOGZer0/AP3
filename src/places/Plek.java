ba51e88d83518597c0e4d0a48f9f17b37407aec1
TheOGZer0 <maartenjeu@gmail.com>
2021-06-06 19:32:14 +0200

    Add JavaDoc to Plek class, attributes, compareTo()

--- src/places/Plek.java
+++ src/places/Plek.java
@@ -7,10 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Representation of a "Place", to be used as Node in a graph with the purpose of being
+ * traversed by Dijkstra's algorithm.
+ */
 public class Plek implements Comparable<Plek>{
     private final String name;
+
+    /** Edges (connecting to other Plek instances) connected to this instance.*/
     private List<Stap> edges = new ArrayList<>();
+
+    /** Shortest path found from start (as specified in {@link graphs.DijkstraGraph#shortestPath(Plek, Plek)}).
+    Initialized as non-specified {@link Reis}. Only supposed to be used by DijkstraGraph. */
     private Reis shortestPath = new Reis(true);
+
+    /** Boolean wether all possible edges of this node have been explored by
+     * {@link graphs.DijkstraGraph#shortestPath(Plek, Plek)}.
+     * Initialized as false. Only supposed to be used by DijkstraGraph.*/
     private boolean completed = false;
 
     public Plek(String name){this.name = name;}
@@ -32,11 +45,19 @@ public class Plek implements Comparable<Plek>{
     public boolean getCompleted(){return this.completed;}
     public void setCompleted(boolean completed){this.completed = completed;}
 
+    /**
+     * Compare plek instances by comparing their associated shortestJourney using {@link Reis#compareTo(Reis)}.
+     *
+     * @param o other Plek instance to compare to.
+     * @return negative integer if instance method is called on is smaller the parameter instance,
+     * positive integer if bigger, 0 if they are equal.
+     */
     @Override
     public int compareTo(Plek o) {
         return this.shortestPath.compareTo(o.getShortestPath());
     }
 
+    // Equals and hashCode based on name.
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
