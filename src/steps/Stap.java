234f2f79bb74155ab1a96167c1f0310713a1dc2a
TheOGZer0 <maartenjeu@gmail.com>
2021-06-06 03:59:28 +0200

    Add JavaDoc to Stap class, Constructor, getOther(), verifyWeight()

--- src/steps/Stap.java
+++ src/steps/Stap.java
@@ -3,10 +3,20 @@ package steps;
 import misc.DijkstraError;
 import places.Plek;
 
+/**
+ * Abstract "Stap", that's meant as edge in a graph to be traversed using Dijkstra's algorithm.
+ */
 public abstract class Stap{
     protected final Plek a;
     protected final Plek b;
 
+    /**
+     * Constructor, that also calls .connect() on a and b, with itself as parameter.
+     *
+     * @param a node at end of edge.
+     * @param b node at end of edge.
+     * @throws DijkstraError called by subclass constructors.
+     */
     public Stap(Plek a, Plek b) throws DijkstraError{
         this.a = a;
         this.b = b;
@@ -14,6 +24,12 @@ public abstract class Stap{
         b.connect(this);
     }
 
+    /**
+     * Given Node at 1 end of edge, return other.
+     *
+     * @param origin node, of which node on other side should be returned.
+     * @return other node.
+     */
     public Plek getOther(Plek origin){
         if(origin == this.a){
             return this.b;
@@ -25,6 +41,12 @@ public abstract class Stap{
     public abstract String toString();
     public abstract double getWeight();
 
+    /**
+     * Verify that edge doesn't have negative weight.
+     * Used by subclasses.
+     *
+     * @throws DijkstraError if weight is negative.
+     */
     protected void verifyWeight() throws DijkstraError{
         if(this.getWeight() < 0){
             throw new DijkstraError(this);
