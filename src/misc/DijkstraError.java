e67cfd01d5e312fa58bf4ae26d00d430a97ab236
TheOGZer0 <maartenjeu@gmail.com>
2021-06-06 22:03:43 +0200

    Add JavaDoc to DijkstraError class, constructor

--- src/misc/DijkstraError.java
+++ src/misc/DijkstraError.java
@@ -3,11 +3,19 @@ package misc;
 import graphs.DijkstraGraph;
 import steps.Stap;
 
+/**
+ * Custom exception to help with Dijkstra-specific problems.
+ */
 public class DijkstraError extends Exception{
     public DijkstraError(String message){
         super(message);
     }
 
+    /**
+     * Constructor for when an edge (Stap) has a negative weight.
+     *
+     * @param edge Stap instance with negative weight.
+     */
     public DijkstraError(Stap edge){
         this("Edge " + edge.toString() + " can't have negative weight.");
     }
