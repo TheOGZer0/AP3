86ccad805ee021c001eb823ca671b78bc42b2813
TheOGZer0 <maartenjeu@gmail.com>
2021-06-07 17:06:18 +0200

    Add JavaDoc to DijkstraGraph class, constructors, attributes, shortestPath()

--- src/graphs/DijkstraGraph.java
+++ src/graphs/DijkstraGraph.java
@@ -10,9 +10,19 @@ import java.util.PriorityQueue;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Graph for shortest-path finding purposes using Dijkstra's algorithm.
+ */
 public class DijkstraGraph{
+    /**
+     * All nodes to be traversed by the algorhitm.
+     */
     private Set<Plek> nodes = new HashSet<>();
 
+    /**
+     * Reset the shortestPath and completed attributes back to their original values
+     * ,after {@link DijkstraGraph#shortestPath(Plek, Plek)} has changed them.
+     */
     private void reset(){
         for(Plek node: this.nodes){
             node.setShortestPath(new Reis(true));
@@ -20,17 +30,34 @@ public class DijkstraGraph{
         }
     }
 
+    /**
+     * @param node add to {@link DijkstraGraph#nodes}.
+     */
     public void addToNodes(Plek node){
         this.nodes.add(node);
     }
 
     public DijkstraGraph(){};
+
+    /**
+     * DijkstraGraph Constructor.
+     *
+     * @param nodes calls {@link DijkstraGraph#addToNodes(Plek)} with all Plek instances that are parameters.
+     */
     public DijkstraGraph(Plek... nodes){
         for(Plek node: nodes){
             this.addToNodes(node);
         }
     }
 
+    /**
+     * Find the shortest path through all nodes/Plek instances, from a given start to a given end.
+     *
+     * @param start Node from which path should be searched.
+     * @param end Node to which path should be searched.
+     * @return The shortest path as Reis instance.
+     * @throws DijkstraError thrown by {@link Reis#addEdge(Stap)}.
+     */
     public Reis shortestPath(Plek start, Plek end) throws DijkstraError {
         // Initialize everything for Dijkstra
         start.setShortestPath(new Reis());
